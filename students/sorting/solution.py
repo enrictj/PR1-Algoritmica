173f685721ac6914ab282c1b27c55e5c3fca6378
enrictj <enrictomasjulia@gmail.com>
2026-04-05 13:12:10 +0200

    problema 1 casi acabat

--- students/sorting/solution.py
+++ students/sorting/solution.py
@@ -6,8 +6,23 @@ from pathlib import Path
 
 
 def my_sort(arr):
-    raise NotImplementedError("Implement Me!")
-
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = my_sort(arr[:mid])
+    right = my_sort(arr[mid:])
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
 
 def main():
     parser = argparse.ArgumentParser()
