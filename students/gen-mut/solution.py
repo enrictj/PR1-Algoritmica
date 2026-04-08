12ddb47c31f13e517d3d578eb5eb7d29eaeb6f51
Pau Ribalta <pauribaltagabarro@gmail.com>
2026-04-08 19:53:25 +0200

    Exercici 2

--- students/gen-mut/solution.py
+++ students/gen-mut/solution.py
@@ -10,7 +10,49 @@ def dist(p1, p2):
 
 
 def my_gen_mut_algorithm(points):
-    raise NotImplementedError("Implement Me!")
+    if len(points) < 2:
+        return float('inf'), None
+    
+    if len(points) == 2:
+        return dist(points[0], points[1]), (points[0], points[1])
+
+    def get_x(points): 
+        return points[0] 
+    points_sorted = sorted(points, key=get_x)
+
+    mid = len(points_sorted) // 2
+    left = points_sorted[:mid]
+    right = points_sorted[mid:]
+
+    dist_left, pair_left = my_gen_mut_algorithm(left)
+    dist_right, pair_right = my_gen_mut_algorithm(right)
+
+    if dist_left < dist_right:
+        min_dist = dist_left
+        closest_pair = pair_left
+    else:
+        min_dist = dist_right
+        closest_pair = pair_right
+
+
+    mid_x = points_sorted[mid][0]
+    strip = [p for p in points_sorted if abs(p[0] - mid_x) < min_dist]
+
+    def get_y(point):
+        return point[1] 
+    strip.sort(key=get_y)
+
+    for i in range(len(strip)): #Mirar i entendre si es pot optimitzar o com
+        for j in range(i+1, len(strip)):
+            if strip[j][1] - strip[i][1] >= min_dist:
+                break
+            d = dist(strip[i], strip[j])
+            if d < min_dist:
+                min_dist = d
+                closest_pair = (strip[i], strip[j])
+
+    return min_dist, closest_pair
+    
 
 
 def main():
